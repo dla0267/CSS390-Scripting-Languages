30fb829d1514af12c3d4ecfdbcc0028cfbf920a9
dla0267 <dla0267@gmail.com>
2018-11-26 22:38:16 -0800

    adf

---
+++ Assignment4/timeserver/collector.py
@@ -0,0 +1,27 @@
+import urllib2
+import sys
+import os.path
+from time import sleep
+
+interval = 10.0
+outpath = 'output.tsv'
+for num, val in enumerate(sys.argv, start=1):
+    if num == len(sys.argv):
+        break
+    if val == '--interval':
+        interval = float(sys.argv[num])
+
+address = sys.argv[1] + '/stats'
+if os.path.isfile(outpath):
+    results = open(outpath, 'a')
+else:
+    results = open(outpath, 'w')
+while True:
+    response = urllib2.urlopen(address)
+    line = response.read().split("\n")
+    del line[len(line) - 1]
+    for stuff in line:
+        digit = stuff.split(": ")
+        results.write(digit[1] + "\t")  # grab the value, not the string saying what it is.
+    results.write("\n")
+    sleep(interval)
---
+++ Assignment4/timeserver/plot.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+gnuplot <<- EOF
+	set title "Time server status over time"
+	set xlabel "Unix Time"
+	set ylabel "RPS (1-minute rate)"
+	set autoscale
+	set term png
+	set output "result.png"
+	plot 'comp.txt' using 1:2 title '500s' with lines,\
+	'comp.txt' using 1:3 title '200s' with lines,\
+	'comp.txt' using 1:4 title '404s' with lines
+EOF
+rm comp.txt
